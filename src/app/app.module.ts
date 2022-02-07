8210ff4c9ca36b5e2a77b0c4a304f859a3344ac8
William Wilches T <wwilchest@outlook.com>
2022-02-07 15:56:43 -0500

    primercommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+
+
+
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador/contador.module';
+
+
 @NgModule({
   declarations: [
     AppComponent
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
